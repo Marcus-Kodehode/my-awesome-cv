7ae4341ab573520a1d692e29cf24b119edde9728
Marcus <marcus29sfj@gmail.com>
2025-09-05 18:56:49 +0200

    Drag & drop without drama

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,12 @@
 import './globals.css';
 import LayoutWrapper from '@/components/layout/LayoutWrapper';
+import { Analytics } from "@vercel/analytics/next"
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="bg-space text-star">
+        <Analytics/>
         <LayoutWrapper>{children}</LayoutWrapper>
       </body>
     </html>
